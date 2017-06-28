2f33c3d9f8bc574aef66a7f2de54939823f6b715
yanchanglu <513894105@qq.com>
2017-06-28 15:34:52 +0800

    首次提交

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <MHeader></MHeader>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
+  export default {
+    name: 'app',
+    components: {
+      MHeader,
+      Tab
+    }
+  }
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
