497467833ee6cfd428bc8b55e7b974e26e081be9
yanchanglu <513894105@qq.com>
2017-06-30 15:00:26 +0800

    recommend界面开发完成，该分支结束

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -16,7 +16,7 @@
           <ul>
             <li v-for="item in discList" class="item">
               <div class="icon">
-                <img width="60" height="60" :src="item.imgurl" alt="">
+                <img width="60" height="60" v-lazy="item.imgurl" alt="">
               </div>
               <div class="text">
                 <h2 class="name" v-html="item.creator.name"></h2>
@@ -26,19 +26,23 @@
           </ul>
         </div>
       </div>
+      <div class="loading-container" v-show="!discList.length">
+        <loading></loading>
+      </div>
     </scroll>
   </div>
 </template>
 <script>
   import Slider from 'base/slider/slider'
   import Scroll from 'base/scroll/scroll'
+  import Loading from 'base/loading/loading'
   import {getRecommend, getDiscList} from 'api/recommend'
   import {ERR_OK} from 'api/config'
   export default {
     data() {
       return {
         recommends: [],
-        discList: null
+        discList: []
       }
     },
     created() {
@@ -70,7 +74,8 @@
     },
     components: {
       Slider,
-      Scroll
+      Scroll,
+      Loading
     }
   }
 </script>
@@ -125,6 +130,12 @@
             }
           }
         }
+      }
+      .loading-container{
+        position: absolute;
+        width: 100%;
+        top: 50%;
+        transform: translateY(-50%);
       }
     }
   }
