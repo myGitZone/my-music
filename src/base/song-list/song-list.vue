2dab4d55c5327f8f988adcc8033a56f9a91f269f
yanchanglu <513894105@qq.com>
2017-09-13 10:11:57 +0800

    添加图片，升级webpack3

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="song-list">
     <ul>
-      <li v-for="(song,index) in songs" class="item" @click="selectItem(song, index)">
+      <li v-for="(song,index) in songs" class="item" @click="selectItem(song, index)" :key="index">
         <div class="rank" v-show="rank">
           <span :class="getrankCls(index)">{{getRankText(index)}}</span>
         </div>
@@ -14,89 +14,89 @@
   </div>
 </template>
 <script>
-  export default {
-    props: {
-      songs: {
-        type: Array,
-        default: []
-      },
-      rank: {
-        type: Boolean,
-        default: false
+export default {
+  props: {
+    songs: {
+      type: Array,
+      default: []
+    },
+    rank: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    selectItem(item, index) {
+      this.$emit('select', item, index)
+    },
+    getDesc(song) {
+      return `${song.singer}·${song.album}`
+    },
+    getrankCls(index) {
+      if (index <= 2) {
+        return `icon icon${index}`
+      } else {
+        return 'text'
       }
     },
-    methods: {
-      selectItem(item, index) {
-        this.$emit('select', item, index)
-      },
-      getDesc(song) {
-        return `${song.singer}·${song.album}`
-      },
-      getrankCls(index) {
-        if (index <= 2) {
-          return `icon icon${index}`
-        } else {
-          return 'text'
-        }
-      },
-      getRankText(index) {
-        if (index > 2) {
-          return index + 1
-        }
+    getRankText(index) {
+      if (index > 2) {
+        return index + 1
       }
     }
   }
+}
 </script>
 <style lang="scss" scoped>
-  @import "../../common/scss/variable";
-  @import "../../common/scss/mixin";
+@import "../../common/scss/variable";
+@import "../../common/scss/mixin";
 
-  .song-list {
-    .item {
-      display: flex;
-      align-items: center;
-      box-sizing: border-box;
-      height: 64px;
-      font-size: $font-size-medium;
-      .rank {
-        flex: 0 0 25px;
+.song-list {
+  .item {
+    display: flex;
+    align-items: center;
+    box-sizing: border-box;
+    height: 64px;
+    font-size: $font-size-medium;
+    .rank {
+      flex: 0 0 25px;
+      width: 25px;
+      margin-right: 30px;
+      text-align: center;
+      .icon {
+        display: inline-block;
         width: 25px;
-        margin-right: 30px;
-        text-align: center;
-        .icon {
-          display: inline-block;
-          width: 25px;
-          height: 24px;
-          background-size: 25px 24px;
-          &.icon0 {
-            @include bg-image('first')
-          }
-          &.icon1 {
-            @include bg-image('second')
-          }
-          &.icon2 {
-            @include bg-image('third')
-          }
+        height: 24px;
+        background-size: 25px 24px;
+        &.icon0 {
+          @include bg-image('first')
         }
-        .text {
-          color: $color-theme;
-          font-size: $font-size-large;
+        &.icon1 {
+          @include bg-image('second')
+        }
+        &.icon2 {
+          @include bg-image('third')
         }
       }
-      .content {
-        flex: 1;
-        line-height: 20px;
-        overflow: hidden;
-        .name {
-          @include no-wrap();
-          color: $color-text;
-        }
-        .desc {
-          @include no-wrap();
-          margin-top: 4px;
-          color: $color-text-d;
-        }
+      .text {
+        color: $color-theme;
+        font-size: $font-size-large;
+      }
+    }
+    .content {
+      flex: 1;
+      line-height: 20px;
+      overflow: hidden;
+      .name {
+        @include no-wrap();
+        color: $color-text;
+      }
+      .desc {
+        @include no-wrap();
+        margin-top: 4px;
+        color: $color-text-d;
       }
     }
   }
+}
 </style>
